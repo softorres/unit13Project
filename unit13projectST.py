47abe5f7bded3151c02ffaf3bab0fdeacd00ca89
softorres <101760901+softorres@users.noreply.github.com>
2023-11-29 21:28:09 -0500

    Update unit13projectST.py

--- unit13projectST.py
+++ unit13projectST.py
@@ -8,19 +8,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# ALG1: Insertion Sort
-# def insertion_sort(A):
-#     start_time = time.time()
-#     for j in range(1, len(A)):
-#         key = A[j]
-#         i = j - 1
-#         while i >= 0 and A[i] > key:
-#             A[i + 1] = A[i]
-#             i = i - 1
-#         A[i + 1] = key
-#     end_time = time.time()
-#     return (end_time - start_time) * 1000  # Convert to milliseconds
-
 def insertion_sort(A, n):
     start_time = time.time()
     for j in range(1, n):
